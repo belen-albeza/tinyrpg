07bb93ece18b937e6ad148240cd3c4d68e8d75bb
Belén Albeza <belen.albeza@gmail.com>
2013-02-08 21:47:08 +0000

    refactor map creation

--- js/game.js
+++ js/game.js
@@ -1,43 +1,39 @@
 function Map(size) {
   var scope = this;
+  generateGeometry();
 
-  console.log(GraphNode);
-  console.log(GraphNode.TYPE_START);
-  
-  var colors = [];
-  colors[GraphNode.TYPE_START] =  0xffffff;
-  colors[GraphNode.TYPE_END] = 0x00ffff;
-  colors[GraphNode.TYPE_TREASURE] = 0xffff00;
-  colors[GraphNode.TYPE_BOSS] =  0xff0000;
-  colors[GraphNode.TYPE_LOCK] =  0xcccccc;
-  colors[GraphNode.TYPE_SHOP] =  0x00ff0;
+  function generateGeometry() {
+    var colors = [];
+    colors[GraphNode.TYPE_START] =  0xffffff;
+    colors[GraphNode.TYPE_END] = 0x00ffff;
+    colors[GraphNode.TYPE_TREASURE] = 0xffff00;
+    colors[GraphNode.TYPE_BOSS] =  0xff0000;
+    colors[GraphNode.TYPE_LOCK] =  0xcccccc;
+    colors[GraphNode.TYPE_SHOP] =  0x00ff0;
 
-  this.graph = new Graph();
-  this.graph.generate();
+    scope.graph = new Graph();
+    scope.graph.generate();
 
+    var size = 50;
+    var quad = new THREE.CubeGeometry(size, size, size);
 
-  var size = 50;
-  var quad = new THREE.CubeGeometry(size, size, size);
+    scope.rootDrawable = new THREE.Object3D();
+    var offsetX = 0;
+    scope.graph.nodes.forEach(function(node) {
+      var material = new THREE.MeshBasicMaterial({
+        color: colors[node.type]});
+      var drawable = new THREE.Mesh(quad, material);
+      drawable.position.set(offsetX, 0, 1);
+      scope.rootDrawable.add(drawable);
 
-  this.rootDrawable = new THREE.Object3D();
-  var offsetX = 0;
-  console.log(scope.graph.nodes);
-  scope.graph.nodes.forEach(function(node) {
-    var material = new THREE.MeshBasicMaterial({
-      color: colors[node.type]});
-    var drawable = new THREE.Mesh(quad, material);
-    drawable.position.set(offsetX, 0, 1);
-    scope.rootDrawable.add(drawable);
+      offsetX += (node.distance + 1) * size;
+    });
 
-    console.log(node.type, offsetX);
-
-    offsetX += (node.distance + 1) * size;
-  });
-
-  var line = new THREE.Mesh(new THREE.CubeGeometry(offsetX, size / 4, size),
-                            new THREE.MeshBasicMaterial({color: 0xaaaaaa}));
-  line.position.x = offsetX / 2 - size / 2;
-  scope.rootDrawable.add(line);
+    var line = new THREE.Mesh(new THREE.CubeGeometry(offsetX, size / 4, size),
+                              new THREE.MeshBasicMaterial({color: 0x666666}));
+    line.position.x = offsetX / 2 - size / 2;
+    scope.rootDrawable.add(line);
+  }
 }
 
 
