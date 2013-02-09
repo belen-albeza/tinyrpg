4efd19967f4f83b1aadfd16f735447a4e44685d5
Belén Albeza <belen.albeza@gmail.com>
2013-02-09 12:40:47 +0000

    hero energy events

--- js/logic/hero.js
+++ js/logic/hero.js
@@ -1,6 +1,7 @@
 function Hero( map ) {
   var scope = this;
   EventDispatcher.call(this);
+  scope.MAX_ENERGY = 100;
   scope.positionIndex = 0;
 
   scope.sprite = new HeroSprite( map.tileSize );
@@ -14,10 +15,38 @@ function Hero( map ) {
     money: 0
   };
 
+  scope.energy = scope.MAX_ENERGY;
+
+  function addEnergy(value) {
+    var oldEnergy = scope.energy;
+
+    scope.energy += value;
+    scope.energy = Math.max(0, Math.min(scope.energy, scope.MAX_ENERGY));
+    if (scope.energy <= 0) {
+      die();
+    }
+
+    if (oldEnergy != scope.energy) {
+      scope.dispatchEvent({
+        type: 'statsChanged',
+        hero: scope
+      });
+    }
+  }
+
+  function die() {
+    console.log('DEATH');
+    scope.dispatchEvent({
+      type: 'heroDied',
+      hero: scope
+    });
+  }
+
   this.setPositionIndex = function(index) {
     if( ! map.isValidPosition( index ) ) {
       return;
     }
+
     scope.positionIndex = index;
     scope.sprite.position.x = index * map.tileSize;
 
@@ -30,14 +59,17 @@ function Hero( map ) {
 
   this.moveForward = function() {
     this.setPositionIndex( scope.positionIndex + 1);
+    addEnergy(-1);
   };
 
   this.moveBackwards = function() {
     this.setPositionIndex( scope.positionIndex - 1);
+    addEnergy(-1);
   };
 
   this.earnMoney = function(amount) {
     scope.inventory.money += amount;
     console.log('money', scope.inventory.money);
   };
+
 }
